9e99e915ca41c0b9fec8eb6233749070f681d498
WimBast <wim.bast@gmail.com>
2025-01-30 15:44:23 +0100

    or )

--- src/main/java/org/modelingvalue/logic/impl/OrImpl.java
+++ src/main/java/org/modelingvalue/logic/impl/OrImpl.java
@@ -63,7 +63,7 @@ public final class OrImpl extends AndOrImpl {
 
     @Override
     public String toString(StructureImpl<?> parent) {
-        return parent instanceof OrImpl ? predicate1().toString(this) + " | " + predicate2().toString(this) + ")" : toString();
+        return parent instanceof OrImpl ? predicate1().toString(this) + " | " + predicate2().toString(this) : toString();
     }
 
     @Override
