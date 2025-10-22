bcbceae7f4aaecf6e11bb2d1aef84f342cb83e2e
WimBast <wim.bast@gmail.com>
2025-10-22 13:58:24 +0200

    refactor: replace Console class with Editor class for enhanced functionality and improved user interface

--- src/main/java/org/modelingvalue/nelumbo/Editor.java
+++ src/main/java/org/modelingvalue/nelumbo/Editor.java
@@ -20,6 +20,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Insets;
 import java.awt.Taskbar;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
@@ -30,65 +31,40 @@ import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.io.Serial;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.swing.*;
-import javax.swing.event.CaretEvent;
-import javax.swing.event.CaretListener;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import javax.swing.text.BadLocationException;
-import javax.swing.text.Caret;
 import javax.swing.text.DefaultEditorKit;
 import javax.swing.text.DefaultHighlighter.DefaultHighlightPainter;
-import javax.swing.text.Element;
-import javax.swing.text.Highlighter;
 import javax.swing.text.TextAction;
 
 import org.modelingvalue.nelumbo.integers.Integer;
 import org.modelingvalue.nelumbo.syntax.ParseException;
 import org.modelingvalue.nelumbo.syntax.Parser;
-import org.modelingvalue.nelumbo.syntax.Token;
 import org.modelingvalue.nelumbo.syntax.Tokenizer;
 
-public class Console extends WindowAdapter implements WindowListener, ActionListener, Runnable, DocumentListener, CaretListener {
+public class Editor extends WindowAdapter implements WindowListener, ActionListener, Runnable, DocumentListener {
 
-    private final boolean                        COLOR_CONSOLE    = java.lang.Boolean.getBoolean("COLOR_CONSOLE_NELUMBO");
+    private final static String                  INCREASE   = "INCREASE";
+    private final static String                  DECREASE   = "DECREASE";
 
-    private final static int                     COMMENT_POSITION = 32;
-    private final static String                  PREFIX           = "  ";
-    //
-    private final static String                  INCREASE         = "INCREASE";
-    private final static String                  DECREASE         = "DECREASE";
-    private final static String                  UP_HISTORY       = "UP_HISTORY";
-    private final static String                  DOWN_HISTORY     = "DOWN_HISTORY";
-    //
-    private final static DefaultHighlightPainter whitePainter     = new DefaultHighlightPainter(new Color(0xffffff));
-    private final static DefaultHighlightPainter lightGreyPainter = new DefaultHighlightPainter(new Color(0xdddddd));
-    private final static DefaultHighlightPainter greyPainter      = new DefaultHighlightPainter(new Color(0xaaaaaa));
-    private final static DefaultHighlightPainter bluePainter      = new DefaultHighlightPainter(new Color(0x88ffff));
-    private final static DefaultHighlightPainter pinkPainter      = new DefaultHighlightPainter(new Color(0xff88ff));
-    private final static DefaultHighlightPainter yellowPainter    = new DefaultHighlightPainter(new Color(0xffff88));
-    private final static DefaultHighlightPainter purplePainter    = new DefaultHighlightPainter(new Color(0x8888ff));
-    private final static DefaultHighlightPainter greenPainter     = new DefaultHighlightPainter(new Color(0x88ff88));
-    private final static DefaultHighlightPainter redPainter       = new DefaultHighlightPainter(new Color(0xff8888));
+    private final static DefaultHighlightPainter redPainter = new DefaultHighlightPainter(new Color(0xff8888));
 
     public static void main(String[] arg) {
-        new Console();
+        new Editor();
     }
 
     //===========================================================================================================================================
-    private JFrame             frame;
-    private JTextArea          textArea;
-    private boolean            quit                 = false;
-    private int                lineCount            = -1;
-    private KnowledgeBase      knowledgeBase;
-    private Action             deletePreviousAction;
-    private final List<String> lineHistory          = new ArrayList<>();
-    private int                currentLineInHistory = 0;
+    private JFrame        frame;
+    private JTextArea     textArea;
+    private boolean       quit = false;
+    private KnowledgeBase knowledgeBase;
+    private Action        deletePreviousAction;
+    private JTextArea     message;
 
-    public Console() {
+    public Editor() {
         initWindow();
         initActions();
         initKnowledgeBase();
@@ -115,13 +91,26 @@ public class Console extends WindowAdapter implements WindowListener, ActionList
         frame.setBounds(x, y, frameSize.width, frameSize.height);
 
         textArea = new JTextArea();
-        textArea.setFont(new Font(Font.MONOSPACED, Font.PLAIN, textArea.getFont().getSize() + 2));
+        Font font = new Font(Font.MONOSPACED, Font.PLAIN, textArea.getFont().getSize() + 2);
+        textArea.setFont(font);
         textArea.setEditable(true);
-        textArea.addCaretListener(this);
+        Insets margin = (Insets) textArea.getMargin().clone();
+        margin.set(margin.top, margin.left + 4, margin.bottom, margin.right);
+        textArea.setMargin(margin);
+
         JButton clear = new JButton("clear");
 
+        message = new JTextArea("");
+        message.setEditable(false);
+        message.setFont(font);
+        message.setMargin(margin);
+
         frame.getContentPane().setLayout(new BorderLayout());
-        frame.getContentPane().add(new JScrollPane(textArea), BorderLayout.CENTER);
+        JSplitPane split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, //
+                new JScrollPane(textArea), //
+                new JScrollPane(message));
+        split.setDividerLocation(frameSize.width / 4 * 3);
+        frame.getContentPane().add(split, BorderLayout.CENTER);
         frame.getContentPane().add(clear, BorderLayout.SOUTH);
         frame.setVisible(true);
 
@@ -140,9 +129,6 @@ public class Console extends WindowAdapter implements WindowListener, ActionList
         textArea.getInputMap().put(KeyStroke.getKeyStroke('=', InputEvent.META_DOWN_MASK), INCREASE);
         textArea.getInputMap().put(KeyStroke.getKeyStroke('-', InputEvent.META_DOWN_MASK), DECREASE);
         textArea.getInputMap().put(KeyStroke.getKeyStroke('_', InputEvent.META_DOWN_MASK), DECREASE);
-
-        textArea.getInputMap().put(KeyStroke.getKeyStroke("UP"), UP_HISTORY);
-        textArea.getInputMap().put(KeyStroke.getKeyStroke("DOWN"), DOWN_HISTORY);
 
         textArea.getActionMap().put(INCREASE, new TextAction(INCREASE) {
             @Serial
@@ -185,38 +171,7 @@ public class Console extends WindowAdapter implements WindowListener, ActionList
             @Override
             public void actionPerformed(ActionEvent e) {
                 if (textArea == getTextComponent(e)) {
-                    Caret caret = textArea.getCaret();
-                    if (getStart() + PREFIX.length() < caret.getMark()) {
-                        deletePreviousAction.actionPerformed(e);
-                    }
-                }
-            }
-        });
-        textArea.getActionMap().put(UP_HISTORY, new TextAction(UP_HISTORY) {
-            @Serial
-            private static final long serialVersionUID = -8886848398049220330L;
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (textArea == getTextComponent(e)) {
-                    if (currentLineInHistory > 0) {
-                        String line = lineHistory.get(--currentLineInHistory);
-                        textArea.replaceRange(line, getStart() + PREFIX.length(), getEnd() - 1);
-                    }
-                }
-            }
-        });
-        textArea.getActionMap().put(DOWN_HISTORY, new TextAction(DOWN_HISTORY) {
-            @Serial
-            private static final long serialVersionUID = -5674886754830419207L;
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (textArea == getTextComponent(e)) {
-                    if (currentLineInHistory < lineHistory.size()) {
-                        String line = lineHistory.get(currentLineInHistory++);
-                        textArea.replaceRange(line, getStart() + PREFIX.length(), getEnd() - 1);
-                    }
+                    deletePreviousAction.actionPerformed(e);
                 }
             }
         });
@@ -236,13 +191,17 @@ public class Console extends WindowAdapter implements WindowListener, ActionList
     private void increase() {
         Font font = textArea.getFont();
         float newSize = Math.min(100f, font.getSize() * 1.2f);
-        textArea.setFont(font.deriveFont(newSize));
+        font = font.deriveFont(newSize);
+        textArea.setFont(font);
+        message.setFont(font);
     }
 
     private void decrease() {
         Font font = textArea.getFont();
         float newSize = Math.max(7f, font.getSize() / 1.2f);
-        textArea.setFont(font.deriveFont(newSize));
+        font = font.deriveFont(newSize);
+        textArea.setFont(font);
+        message.setFont(font);
     }
 
     @Override
@@ -267,124 +226,70 @@ public class Console extends WindowAdapter implements WindowListener, ActionList
         textArea.getHighlighter().removeAllHighlights();
         knowledgeBase.init();
         textArea.setText("");
-        prepareRead();
-    }
-
-    private void prepareRead() {
-        lineCount = textArea.getLineCount();
-        textArea.append(PREFIX);
-        textArea.setCaretPosition(textArea.getDocument().getLength());
-    }
-
-    private void write(String output) {
-        int pos = Math.max(1, COMMENT_POSITION - getLength());
-        textArea.insert(" ".repeat(pos) + "// " + output, getEnd() - 1);
-    }
-
-    private void error(String error) {
-        int pos = Math.max(1, COMMENT_POSITION - getLength());
-        textArea.insert(" ".repeat(pos) + "// " + error, getEnd() - 1);
+        message.setText("");
     }
 
     @Override
     public synchronized void run() {
         knowledgeBase = KnowledgeBase.CURRENT.get();
-        for (String line = readLine(); line != null; line = readLine()) {
-            lineHistory.add(line.substring(PREFIX.length(), line.length() - 1));
-            currentLineInHistory = lineHistory.size();
-            executeLine(line);
+        String pre = "";
+        for (String text = read(); text != null; text = read()) {
+            if (!pre.equals(text)) {
+                execute(text);
+                pre = text;
+            }
         }
     }
 
-    private String readLine() {
-        prepareRead();
+    private String read() {
         while (!quit) {
             try {
                 wait(1_000_000);
             } catch (InterruptedException ie) {
                 // ignore
             }
-            if (lineCount < textArea.getLineCount() && 0 < getEnd()) {
-                try {
-                    return textArea.getText(getStart(), getLength());
-                } catch (BadLocationException ble) {
-                    error(ble.getMessage());
-                }
-            }
+            return textArea.getText();
         }
         return null;
     }
 
-    private void executeLine(String line) {
+    private void execute(String text) {
+        message.setText("");
+        textArea.getHighlighter().removeAllHighlights();
+        knowledgeBase.init();
         try {
-            if (COLOR_CONSOLE) {
-                applySyntaxColors(line);
-            }
-            Tokenizer tokenizer = new Tokenizer(line, line);
+            Tokenizer tokenizer = new Tokenizer(text, "Editor");
             Parser parser = new Parser(tokenizer.tokenize());
+            String out = "";
+            int prevLine = 0, nextLine = 0;
             for (Node root : parser.parseMutiple().roots()) {
                 if (root instanceof Query query) {
-                    write(query.inferResult().toString());
+                    nextLine = query.lastToken().line();
+                    out += emptyLines(nextLine - prevLine) + query.inferResult().toString() + "\n";
+                    prevLine = ++nextLine;
                 }
             }
+            output(out);
         } catch (ParseException pe) {
-            error(pe.getShortMessage());
+            error(emptyLines(pe.line()) + pe.getShortMessage());
             try {
-                int start = getStart() + pe.position();
-                textArea.getHighlighter().addHighlight(start, start + pe.length(), redPainter);
+                textArea.getHighlighter().addHighlight(pe.index(), pe.index() + pe.length(), redPainter);
             } catch (BadLocationException ble) {
                 error(ble.getMessage());
             }
         }
     }
 
-    private void applySyntaxColors(String line) {
-        System.err.println("line=[" + U.traceable(line) + "]");
-        textArea.insert(" ", getEnd() - 1);
-        Tokenizer tokenizer = new Tokenizer(line, line);
-        for (Token token = tokenizer.tokenize().firstAll(); token != null; token = token.nextAll()) {
-            try {
-                int beg = getStart() + token.position();
-                int end = beg + token.text().length();
-                System.err.printf("  - [%2d...%2d]: '%s'\n", beg, end, token);
-                Highlighter.HighlightPainter hp = switch (token.type()) {
-                case STRING -> bluePainter;
-                case NUMBER, DECIMAL -> greenPainter;
-                case NAME -> yellowPainter;
-                case SEMICOLON, OPERATOR, LEFT, RIGHT, META_OPERATOR -> greyPainter;
-                case COMMA -> purplePainter;
-                case TYPE -> pinkPainter;
-                case END_LINE_COMMENT, IN_LINE_COMMENT, SINGLEQUOTE -> lightGreyPainter;
-                case HSPACE, NEWLINE -> whitePainter;
-                case ERROR, ENDOFFILE -> redPainter;
-                };
-                if (hp != null) {
-                    textArea.getHighlighter().addHighlight(beg, end, hp);
-                }
-            } catch (BadLocationException e) {
-                System.err.println("BAD LOCATION: " + token.text() + " " + e.getMessage());
-            }
-        }
+    private String emptyLines(int nr) {
+        return "\n".repeat(nr);
     }
 
-    private int getStart() {
-        Element line = getLastLineElement();
-        return line == null ? 0 : line.getStartOffset();
+    private void error(String msg) {
+        message.setText(msg);
     }
 
-    private int getEnd() {
-        Element line = getLastLineElement();
-        return line == null ? 0 : line.getEndOffset();
-    }
-
-    private int getLength() {
-        Element line = getLastLineElement();
-        return line == null ? 0 : line.getEndOffset() - line.getStartOffset();
-    }
-
-    private Element getLastLineElement() {
-        Element root = textArea.getDocument().getDefaultRootElement();
-        return root.getElementCount() < lineCount ? null : root.getElement(lineCount - 1);
+    private void output(String msg) {
+        message.setText(msg);
     }
 
     @Override
@@ -393,18 +298,13 @@ public class Console extends WindowAdapter implements WindowListener, ActionList
     }
 
     @Override
-    public void removeUpdate(DocumentEvent e) {
+    public synchronized void removeUpdate(DocumentEvent e) {
+        notifyAll();
     }
 
     @Override
-    public void changedUpdate(DocumentEvent e) {
+    public synchronized void changedUpdate(DocumentEvent e) {
+        notifyAll();
     }
 
-    @Override
-    public void caretUpdate(CaretEvent e) {
-        int newPos = getStart() + PREFIX.length();
-        if (getStart() != 0 && e.getMark() < newPos && newPos < textArea.getDocument().getLength()) {
-            textArea.setCaretPosition(newPos);
-        }
-    }
 }
