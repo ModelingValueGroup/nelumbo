a525c71028150b05db47e02ce5fc16f8d76cf354
Tom Brus <tombrus@gmail.com>
2026-03-17 12:56:57 +0100

    Bump `com.gradleup.shadow` plugin version to 9.3.2
    - Updated `com.gradleup.shadow` plugin from 9.3.1 to 9.3.2 in `build.gradle.kts` and `lsp/server/build.gradle.kts`.

--- lsp/server/build.gradle.kts
+++ lsp/server/build.gradle.kts
@@ -17,7 +17,7 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
-    id("com.gradleup.shadow") version "9.3.1"
+    id("com.gradleup.shadow") version "9.3.2"
     java
 }
 
