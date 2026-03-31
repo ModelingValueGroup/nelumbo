2f13fb7d5013e4d14197f128bb2689b3b24a6b29
WimBast <wim.bast@gmail.com>
2026-03-31 09:33:51 +0200

    Fix ParseException to handle empty elements array and improve constructor formatting

--- src/main/java/org/modelingvalue/nelumbo/syntax/ParseException.java
+++ src/main/java/org/modelingvalue/nelumbo/syntax/ParseException.java
@@ -22,114 +22,115 @@ import org.modelingvalue.collections.List;
 import org.modelingvalue.nelumbo.AstElement;
 
 public class ParseException extends Exception {
-    @Serial
-    private static final long serialVersionUID = -8359192414582977261L;
+	@Serial
+	private static final long serialVersionUID = -8359192414582977261L;
 
-    private final int    line;
-    private final int    position;
-    private final int    index;
-    private final int    length;
-    private final String fileName;
+	private final int line;
+	private final int position;
+	private final int index;
+	private final int length;
+	private final String fileName;
 
-    public ParseException(String s, AstElement... elements) {
-        this(null, s, elements);
-    }
+	public ParseException(String s, AstElement... elements) {
+		this(null, s, elements);
+	}
 
-    public ParseException(Throwable cause, String s, AstElement... elements) {
-        this(cause, s, firstToken(elements), lastToken(elements));
-    }
+	public ParseException(Throwable cause, String s, AstElement... elements) {
+		this(cause, s, firstToken(elements), lastToken(elements));
+	}
 
-    public ParseException(String s, List<AstElement> elements) {
-        this(null, s, elements);
-    }
+	public ParseException(String s, List<AstElement> elements) {
+		this(null, s, elements);
+	}
 
-    public ParseException(Throwable cause, String s, List<AstElement> elements) {
-        this(cause, s, firstToken(elements), lastToken(elements));
-    }
+	public ParseException(Throwable cause, String s, List<AstElement> elements) {
+		this(cause, s, firstToken(elements), lastToken(elements));
+	}
 
-    private static Token lastToken(List<AstElement> elements) {
-        return lastToken(elements.toArray(AstElement[]::new));
-    }
+	private static Token lastToken(List<AstElement> elements) {
+		return lastToken(elements.toArray(AstElement[]::new));
+	}
 
-    private static Token firstToken(List<AstElement> elements) {
-        return firstToken(elements.toArray(AstElement[]::new));
-    }
+	private static Token firstToken(List<AstElement> elements) {
+		return firstToken(elements.toArray(AstElement[]::new));
+	}
 
-    private static Token lastToken(AstElement... elements) {
-        Token lastToken = elements[elements.length - 1].lastToken();
-        if (lastToken != null) {
-            while (lastToken.type().isLayout() && lastToken.previous() != null) {
-                lastToken = lastToken.previous();
-            }
-        }
-        return lastToken;
-    }
+	private static Token lastToken(AstElement... elements) {
+		Token lastToken = elements.length > 0 ? elements[elements.length - 1].lastToken() : null;
+		if (lastToken != null) {
+			while (lastToken.type().isLayout() && lastToken.previous() != null) {
+				lastToken = lastToken.previous();
+			}
+		}
+		return lastToken;
+	}
 
-    private static Token firstToken(AstElement... elements) {
-        Token firstToken = elements[0].firstToken();
-        if (firstToken != null) {
-            while (firstToken.type().isLayout() && firstToken.next() != null) {
-                firstToken = firstToken.next();
-            }
-        }
-        return firstToken;
-    }
+	private static Token firstToken(AstElement... elements) {
+		Token firstToken = elements.length > 0 ? elements[0].firstToken() : null;
+		if (firstToken != null) {
+			while (firstToken.type().isLayout() && firstToken.next() != null) {
+				firstToken = firstToken.next();
+			}
+		}
+		return firstToken;
+	}
 
-    private ParseException(Throwable cause, String message, Token firstToken, Token lastToken) {
-        this(cause, //
-             message, //
-             firstToken != null ? firstToken.line() : -1, //
-             firstToken != null ? firstToken.position() : -1, //
-             firstToken != null ? firstToken.index() : -1, //
-             firstToken != null ? lastToken.position() - firstToken.position() + lastToken.text().length() : -1, //
-             firstToken != null ? firstToken.fileName() : ""//
-            );
-    }
+	private ParseException(Throwable cause, String message, Token firstToken, Token lastToken) {
+		this(cause, //
+				message, //
+				firstToken != null ? firstToken.line() : -1, //
+				firstToken != null ? firstToken.position() : -1, //
+				firstToken != null ? firstToken.index() : -1, //
+				firstToken != null ? lastToken.position() - firstToken.position() + lastToken.text().length() : -1, //
+				firstToken != null ? firstToken.fileName() : ""//
+		);
+	}
 
-    public ParseException(String message, String fileName) {
-        this(null, message, fileName);
-    }
+	public ParseException(String message, String fileName) {
+		this(null, message, fileName);
+	}
 
-    public ParseException(Throwable cause, String message, String fileName) {
-        this(cause, message, 0, 0, 0, 0, fileName);
-    }
+	public ParseException(Throwable cause, String message, String fileName) {
+		this(cause, message, 0, 0, 0, 0, fileName);
+	}
 
-    private ParseException(Throwable cause, String message, int line, int position, int index, int length, String fileName) {
-        super(message, cause);
-        this.line     = line;
-        this.position = position;
-        this.index    = index;
-        this.length   = length;
-        this.fileName = fileName;
-    }
+	private ParseException(Throwable cause, String message, int line, int position, int index, int length,
+			String fileName) {
+		super(message, cause);
+		this.line = line;
+		this.position = position;
+		this.index = index;
+		this.length = length;
+		this.fileName = fileName;
+	}
 
-    @Override
-    public String getMessage() {
-        return getShortMessage() + ", line=" + (line + 1) + ", position=" + (position + 1) + ", file=" + fileName;
-    }
+	@Override
+	public String getMessage() {
+		return getShortMessage() + ", line=" + (line + 1) + ", position=" + (position + 1) + ", file=" + fileName;
+	}
 
-    public String getShortMessage() {
-        return super.getMessage();
-    }
+	public String getShortMessage() {
+		return super.getMessage();
+	}
 
-    public int line() {
-        return line;
-    }
+	public int line() {
+		return line;
+	}
 
-    public int position() {
-        return position;
-    }
+	public int position() {
+		return position;
+	}
 
-    public int index() {
-        return index;
-    }
+	public int index() {
+		return index;
+	}
 
-    public int length() {
-        return length;
-    }
+	public int length() {
+		return length;
+	}
 
-    @SuppressWarnings("unused")
-    public String fileName() {
-        return fileName;
-    }
+	@SuppressWarnings("unused")
+	public String fileName() {
+		return fileName;
+	}
 }
